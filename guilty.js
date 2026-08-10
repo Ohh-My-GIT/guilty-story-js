21f45975953ed36058d0065623c95b505f040044
ミラクル吐瀉物ゲロリンチョ <244408662+Ohh-My-GIT@users.noreply.github.com>
2026-08-10 12:08:03 +0900

    書ききった。絵文字追加予定

---
+++ guilty.js
@@ -0,0 +1,100 @@
+// constを持参して興味のあったPEP国へ観光にきたJavaScript。ところが...ある発言で通報されてしまう
+
+
+// PEP警察「JavaScriptさん、それはプロパティでは無くて、キーです。あと、オブジェクトじゃなくてdictです。連れていきますね」
+
+//JavaScript「ハァ？」
+
+//裁判官「Guilty」
+
+//Python「😏😏😏」
+
+//JavaScript「アンタだって、その辞書とやらとテンプレートリテラルでは{}は合法じゃないの！！」
+
+//裁判官「静粛に。それとそれは話が違います」
+
+"Guilty" === "Guilty" // true
+"Guilty == "Guilty" 
+  // true
+
+//Python「救いようないじゃん🤣」
+
+//JavaScript「その指、へし折りたい」
+
+//裁判官「折るぞ、ではないので脅迫罪には問われません」
+
+// ===== ECMA国では　=====
+
+//TypeScript先生「JavaScriptさん、今日も欠席ですか？
+
+//Node「PEP国で、罪に問われて裁判にかけられているそうです」
+
+//TypeScript先生「はぁ...だからあれほどあの国には行くなと言ったのに」
+
+// ===== PEP国では裁判中 =====
+
+const info = {
+  name: "JavaScript",
+  occupation: "プログラミング言語",
+  type: "動的",
+  personality: "非同期",
+  address: "ブラウザやターミナルなど",
+  giveUp: undefined
+};
+
+//裁判官「なるほどねぇ。諦めは悪いようです」
+
+//Python「性格が非同期？ｗ」
+
+//裁判官「つまり、質問にはすぐに答えないという性格ですね」
+
+//JavaScript「笑うな！！」
+
+//Python「返事ほしいならPromiseしなきゃ無視されそうｗｗｗ」
+
+//JavaScript「その顔面、awaitなしで殴りたい」
+
+//裁判官「なるほど、付け足しておきますね」
+
+info.nowMood = "その顔面、awaitなしで殴りたい";
+
+// 裁判官「っと...」
+
+//JavaScript「付け足してどうするんですか！」
+
+//裁判官「一応、証拠なので」
+
+//JavaScript「は？」
+
+//裁判官「法律上の、Promiseなので」
+
+const Judgment = "Death Penalty";
+  console.log(judgment) // ReferenceError
+
+// JavaScript「はぁぁぁぁ、良かった」
+
+let judgment = "Life Imprisonment";
+  console.log(judgment);
+
+//JavaScript「はぁ？」
+
+//裁判官「安心してください。定数ではなく、変数なので」
+
+//JavaScript「確かに...」
+
+//TypeScript先生「型について色々教えることがあるので」
+
+//Node「また、KIttyやWezTeamに連れていきたいので」
+
+//裁判官「しょうがないですねぇ」
+
+judgment = "Parole";
+
+//JavaScript「か、仮？？」
+
+//裁判官「我が国で再犯したら即...あの定数をエラーなしで実行します」
+
+//Python「あんたは、ECMAに帰れ」
+
+//JavaScript「もう、PEPはこりごりだよ！｝
+console.log("おわり");
